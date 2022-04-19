105d26e43325a3be90d958dad60fc6ac78bbc159
nrnidaaulia <nurnidaaulia99@gmail.com>
2022-04-19 17:20:34 +0700

    clean up

--- src/jsx/components/PaginatedTable/CustomerService.js
+++
@@ -1,7 +0,0 @@
-export class CustomerService {
-  getCustomersLarge() {
-    return fetch("../../../constants/customer_large.json")
-      .then((res) => res.json())
-      .then((d) => d.data);
-  }
-}

f73d24697888fc279e28bb2ce1f60d0221cb75b6
FaaaDev <faaadev@gmail.com>
2022-05-09 18:47:06 +0700

    satuan

--- src/jsx/components/Satuan/index.js
+++ src/jsx/components/Satuan/index.js
@@ -3,18 +3,19 @@ import { TabView, TabPanel } from "primereact/tabview";
 import DataSatuan from "./Satuan";
 import Konversi from "./Konversi";
 
-
-
 const Satuan = () => {
-    return <TabView>
-    <TabPanel header="Data Satuan" >
-      <DataSatuan/>
-    </TabPanel>
+  return (
+    <DataSatuan/>
+    // <TabView>
+    //   <TabPanel header="Data Satuan">
+    //     <DataSatuan />
+    //   </TabPanel>
 
-    <TabPanel header="Konversi Satuan">
-      <Konversi/>
-    </TabPanel>
-  </TabView>
-}
+    //   <TabPanel header="Konversi Satuan">
+    //     <Konversi />
+    //   </TabPanel>
+    // </TabView>
+  );
+};
 
-export default Satuan
+export default Satuan;

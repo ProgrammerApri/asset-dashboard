587a19707b1c3d5e686a22c81380375bb12f1559
FaaaDev <faaadev@gmail.com>
2022-06-07 15:04:07 +0700

    empty transaksi persediaan

---
+++ src/jsx/components/Empty/AAA/AAA.js
@@ -0,0 +1,17 @@
+import { TabView, TabPanel } from "primereact/tabview";
+import React from "react";
+import X from "./x";
+
+const AAA = () => {
+  return (
+    <TabView>
+      <TabPanel header="X">
+          <X/>
+      </TabPanel>
+      
+    </TabView>
+  );
+};
+
+
+export default AAA;
